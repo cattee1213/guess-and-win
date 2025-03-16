6918f061be0c9cf05aa2899fca8e337f3158c16d
xsf <fei523688393@hotmail.com>
2025-03-16 11:06:53 +0800

    🐞 fix: test fix

--- programs/guess-and-win/src/program_error.rs
+++ programs/guess-and-win/src/program_error.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum OperationError {
-    #[msg("Can't access this account, no right!")]
+    #[msg("Can't access this account, no rights!")]
     NotOwner,
     #[msg("Not enough SOL to pay!")]
     NotEnoughSOL,
